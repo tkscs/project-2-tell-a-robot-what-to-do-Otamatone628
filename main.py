051974739ff83e88dd437c1b08f9728dd9594f5d
Ali <agordon2026@kehillah.org>
2025-12-04 09:23:58 -0800

    in progress

--- main.py
+++ main.py
@@ -5,4 +5,33 @@ from simulator import robot, FORWARD, BACKWARD, STOP
 # Use robot.left_sonar() and robot.right_sonar() to sense obstacles
 
 # When you're done, close the simulator
+state = "waiting"
+on = True
+
+def sim_ready() :
+    action = input("Would you like to check the sonar (...), move (m), or quit (q)?")
+    if action == "..." :
+        print("Left: ", robot.left_sonar())
+        print("Right: ", robot.right_sonar())
+    elif action == "m" :
+        movement_pattern = input("Would you like to control the robot (c), or send it on a predetermined path (p)?")
+    elif action == "q" :
+        on == False
+    
+
+
+
+while on == True:
+    where = input("Would you like to run the robot in the simulator (s) or in real life (r)?")
+    if where == "r" :
+        state = "robot_ready"
+        robot_ready()
+    elif where == "s" :
+        state = "sim_ready"
+        sim_ready()
+    else:
+        #TODO
+
+
+
 robot.exit()
